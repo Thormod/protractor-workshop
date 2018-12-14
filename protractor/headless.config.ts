3c1c479e98bf2b617ce77f1312a84c56930cab77
Juan Sebastian Zapata Tamayo <jzapatat@pslcol.com.co>
2018-12-14 07:02:50 -0500

    Chrome Headless

---
+++ protractor/headless.config.ts
@@ -0,0 +1,24 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export let config: Config = {
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(0);
+  }
+};
